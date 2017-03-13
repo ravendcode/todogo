10ccaa20f48753b5a2ca1c2ee05d9f98e4490940
ravendcode <ravendcode@gmail.com>
2017-03-13 21:28:47 +0200

    Change web

--- cmd/todong/main.go
+++ cmd/todong/main.go
@@ -15,6 +15,7 @@ func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	// a
 
 	// n := negroni.Classic()
 	// n.Use(negroni.HandlerFunc(middlewares.ContextDbMiddleware))
